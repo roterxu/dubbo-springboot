5ae3b9a1258c6fe872b82eaaa78cbc769759f8da
xujie <97063859@qq.com>
2019-05-30 14:46:30 +0800

    1

--- dubbo-provider/src/main/java/com/xiaoze/provider/service/impl/DemoServiceImpl.java
+++ dubbo-provider/src/main/java/com/xiaoze/provider/service/impl/DemoServiceImpl.java
@@ -5,10 +5,13 @@ import com.xiaoze.api.model.User;
 import com.xiaoze.api.service.DemoService;
 import com.xiaoze.provider.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
 /**
  * DemoServiceImpl
  * 服务提供类
+ *
  * @author xiaoze
  * @date 2018/6/7
  */
@@ -18,6 +21,9 @@ public class DemoServiceImpl implements DemoService {
     @Autowired
     private UserDao userDao;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @Override
     public String sayHello(String name) {
         return "Hello, " + name + " (from Spring Boot)";
@@ -25,13 +31,21 @@ public class DemoServiceImpl implements DemoService {
 
     @Override
     public User detail(Integer id) {
-
-        return userDao.selectById(id);
+        ValueOperations<String, User> operations = redisTemplate.opsForValue();
+        String key = "user" + id;
+        boolean haskey = redisTemplate.hasKey(key);
+        if (haskey) {
+            User user = operations.get(key);
+            System.out.println("redis缓存");
+            return user;
+        } else {
+            User user = new User();
+            user = userDao.selectById(id);
+            System.out.println("mysql数据库");
+            operations.set(key, user);
+            return user;
+        }
     }
 
 
-//    @Override
-//    public User select(Integer id) {
-//        return userDao.selectById(id);
-//    }
 }
